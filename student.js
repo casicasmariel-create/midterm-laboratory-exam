698d45f590dbc92ea7d3922128cf056d6c8798e0
casicasmariel-create <casicasmariel@gmail.com>
2026-04-24 09:10:42 +0800

    Add files via upload

---
+++ student.js
@@ -0,0 +1,9 @@
+let name = "Mariel Casicas";
+let course = "Web Development";
+let yearLevel = "2nd Year";
+let favoriteLanguage = "JavaScript";
+
+console.log("Name: " + name);
+console.log("Course: " + course);
+console.log("Year Level: " + yearLevel);
+console.log("Favorite Programming Language: " + favoriteLanguage);
